8f8afc3cba5fcd59db609a2f53cb317e3ca261f1
Rabia Tüylek <52099703+rabiatuylek@users.noreply.github.com>
2022-08-22 14:46:38 +0300

    Add files via upload

---
+++ 2graph/tut2.c
@@ -0,0 +1,17 @@
+// create graph
+
+void tut2()
+{
+    double x[6] = {1,2,3,3.5,4,5};
+    double y[6] = {1,4,9,12,16,25};
+    
+    TGraph *gr = new TGraph(6,x,y);  // 5 points
+    
+    gr->SetMarkerStyle(5);
+    gr->SetMarkerSize(2);
+    
+    TCanvas *c1 = new TCanvas();
+    
+    gr->Draw("ALP");
+
+}
