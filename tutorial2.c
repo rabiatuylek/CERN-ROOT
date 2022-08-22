2d5796f8faa9ceefc75178d29ae4ca3fca344d2a
Rabia Tüylek <52099703+rabiatuylek@users.noreply.github.com>
2022-08-22 14:45:06 +0300

    Delete tutorial2.c

--- tutorial2.c
+++
@@ -1,9 +0,0 @@
-void tutorial2()
-{
-
-    TH1F *hist = new TH1F("hist", "Histogram", 50, 25, 50);  //determine intervals and start from 25 for x axis
-    
-    TCanvas *c1 = new TCanvas();
-    hist->Draw();     // create Hist
-
-}
